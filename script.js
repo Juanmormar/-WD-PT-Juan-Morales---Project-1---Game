f86dfb9c3bcdc4e5322e5a05c1988d046d7c55ec
Juan Morales Martinez <juanmoralesmartinez@Juans-MacBook-Pro.local>
2023-12-23 16:01:48 +0100

    Day3 MVP Complete

--- script.js
+++ script.js
@@ -1,129 +1,146 @@
-let moveSpeed = 3
-let grativy = 0.5
+
 let superman = document.querySelector('.superman')
 let img = document.getElementById('superman-1')
+let gameOverSound = new Audio('audio/GameOverSound.mp3')
+gameOverSound.volume = 0.4
+let supermanHolds = superman.getBoundingClientRect()
+let background = document.querySelector('.background').getBoundingClientRect();
+let score_val = document.querySelector('.score_val')
+let message = document.querySelector('.message')
+let score_title = document.querySelector('.score_title')
 
-let supemanInPlace = superman.getBoundingClientRect()
-
-
-let background = document.querySelector('.background').getBoundingClientRect()
-
-let scoreValue = document.querySelector('.score_val')
-let gameMessage = document.querySelector('.gameMessage')
-let scoreTitle = document.querySelector('.score_title')
-
-let gameScreen = 'Start'
+let game_state = 'Start'
 img.style.display = 'none'
-gameMessage.classList.add('messageStyle');
-
+let move_speed = 3, grativy = 0.5
 document.addEventListener('keydown', (e) => {
     
-    if(e.key == 'Enter' && gameScreen != 'Game'){
-        document.querySelectorAll('.kryptoBlocks').forEach((e) => {
+    if(e.key === 'Enter' && game_state !== 'Play'){
+        document.querySelectorAll('.kryptoBlock').forEach((e) => {
             e.remove()
-        })
+        });
         img.style.display = 'block'
         superman.style.top = '40vh'
-        gameScreen= 'Game'
-        gameMessage.innerHTML = ''
-        scoreTitle.innerHTML = 'Score : '
-        scoreValue.innerHTML = '0'
-        gameMessage.classList.remove('messageStyle')
-        game();
+        game_state = 'Play'
+        message.innerHTML = ''
+        score_title.innerHTML = 'Score : '
+        score_val.innerHTML = '0'
+        // message.classList.remove('messageStyle')
+        play()
     }
-}) 
+});
 
-function game(){
+
+
+function play(){
+
+
+
     function move(){
-        if(gameScreen != `Game`) return
-        let kryptoBlocks = document.querySelector(`.kryptoblocks`)
-        kryptoBlocks.forEach((element)=>{
-            let kryptoBlocksInPlace = element.getBoundingClientRect()
-            supemanInPlace = superman.getBoundingClientRect()
 
-            if(kryptoBlocksInPlace.right <= 0){
+            backgroundMusic.volume = 0.4
+            backgroundMusic.play()
+  
+
+        
+    
+        if(game_state != 'Play'){
+        
+            backgroundMusic.pause()
+        return}
+
+        let kryptoBlock = document.querySelectorAll('.kryptoBlock')
+        kryptoBlock.forEach((element) => {
+            let kryptoHolds = element.getBoundingClientRect()
+            supermanHolds = superman.getBoundingClientRect()
+
+            if(kryptoHolds.right <= 0){
                 element.remove()
             }else{
-                if(supemanInPlace.left < kryptoBlocksInPlace.left + kryptoBlocksInPlace.width &&
-                    supemanInPlace.left + supemanInPlace.width > kryptoBlocksInPlace.left && 
-                    supemanInPlace.top < kryptoBlocksInPlace.top + kryptoBlocksInPlace.heigth &&
-                    supemanInPlace.top + supemanInPlace.height  > kryptoBlocksInPlace.height){
-                        gameScreen = `GameOver`
-                        gameMessage.innerHTML = `Game Over`.fontcolor(`lawngreen`) + 
-                        `<br>Enter To Try Again`
-                        gameMessage.classList.add(`messageStyle`)
-                        img.style.display = `none`
-                        return
-                    }else{
-                        if(kryptoBlocksInPlace.right < supemanInPlace.left && 
-                            kryptoBlocksInPlace.right + moveSpeed >= supemanInPlace.left
-                            && element.increaseScore == `1` ){
-                                scoreValue.innerHTML=+ scoreValue.innerHTML + 1
-                            }
-                            element.style.left = kryptoBlocksInPlace.left - moveSpeed + `px`
+                if(supermanHolds.left < kryptoHolds.left + kryptoHolds.width 
+                    && supermanHolds.left + supermanHolds.width > kryptoHolds.left && 
+                    supermanHolds.top < kryptoHolds.top + kryptoHolds.height &&
+                    supermanHolds.top + supermanHolds.height > kryptoHolds.top){
+                    game_state = 'End'
+                    message.innerHTML = 'Game Over' + '<br>Press Enter To Try Again'
+                    message.classList.add('messageStyle')
+                    img.style.display = 'none'
+                    gameOverSound.play()
+                    return
+                }else{
+                    if(kryptoHolds.right < supermanHolds.left && 
+                        kryptoHolds.right + move_speed >= supermanHolds.left && 
+                        element.increase_score == '1'){
+                        score_val.innerHTML =+ score_val.innerHTML + 1
+                        
                     }
-
+                    element.style.left = kryptoHolds.left - move_speed + 'px'
+                }
             }
         })
         requestAnimationFrame(move)
     }
     requestAnimationFrame(move)
+
     let supermanGrav = 0
-    function addGrativy(){
-        if(gameScreen != `Game`) return
+
+    
+    function apply_gravity(){
+        if(game_state != 'Play') return
         supermanGrav = supermanGrav + grativy
-        document.addEventListener(`keydown`, (e) => {
-            if(e.key == `ArrowUp` || e.key == ` `){
-                img.src == `images/superman2.png`
+        document.addEventListener('keydown', (e) => {
+            if( e.key == ' '){
+                img.src = 'images/superman2.png'
                 supermanGrav = -7.6
             }
-        })
+        });
 
-        document.addEventListener(`keyup`,(e) => {
-            if(e.key == `ArrowUp` || e.key == ` `){
-                img.src = `images/superman1.png`
+        document.addEventListener('keyup', (e) => {
+            if( e.key == ' '){
+                img.src = 'images/superman1.png'
             }
-        })
-if(supemanInPlace.top <=0 || supemanInPlace.bottom >= background.bottom){
-    gameScreen = `GameOver`
-    gameMessage.style.left= `28vw`
-    window.location.reload()
-    gameMessage.classList.remove(`messageStyle`)
-    return
-}
-superman.style.top = supemanInPlace.top + supermanGrav + `px`
-supemanInPlace = superman.getBoundingClientRect()
-requestAnimationFrame(addGrativy)
+        });
+
+        if(supermanHolds.top <= 0 || supermanHolds.bottom >= background.bottom){
+            game_state = 'End'
+            message.style.left = '28vw'
+            gameOverSound.play()
+            window.location.reload()
+            message.classList.remove('messageStyle')
+            return
+        }
+        superman.style.top = supermanHolds.top + supermanGrav + 'px'
+        supermanHolds = superman.getBoundingClientRect()
+        requestAnimationFrame(apply_gravity)
     }
-requestAnimationFrame(addGrativy)
-let blockSeparation = 0
-let blocksGap = 35
+    requestAnimationFrame(apply_gravity)
 
-function createBlock(){
-    if(gameScreen != `Game`) return
+    let kryptoSeparation = 0
 
-    if(blockSeparation > 115){
-        blockSeparation = 0
-        let blockPosition = Math.floor(Math.random()*43) + 8
-        let kryptoBlocksInverted = document.createElement(`div`)
-        kryptoBlocksInverted.className = `kryptoBlocks`
-        kryptoBlocksInverted.style.top = blockPosition - 70 + `vh`
-        kryptoBlocksInverted.style.left = `100vw`
+    let kryptoGap = 34
 
-        document.body.appendChild(kryptoBlocksInverted)
-        let kryptoBlocks = document.createElement (`div`)
-        kryptoBlocks.className = `kryptoBlocks`
-        kryptoBlocks.style.top = blockPosition + blocksGap + `vh`
-        kryptoBlocks.style.left = `100vw` 
-        kryptoBlocks.increaseScore = `1`
+    function create_pipe(){
+        if(game_state !== 'Play') return;
 
-        document.body.appendChild(kryptoBlocks)
+        if(kryptoSeparation > 115){
+            kryptoSeparation = 0
 
+            let kryptoPosition = Math.floor(Math.random() * 43) + 8
+            let kryptoInverted = document.createElement('div')
+            kryptoInverted.className = 'kryptoBlock'
+            kryptoInverted.style.top = kryptoPosition - 70 + 'vh'
+            kryptoInverted.style.left = '100vw'
+
+            document.body.appendChild(kryptoInverted)
+            let kryptoBlock = document.createElement('div')
+            kryptoBlock.className = 'kryptoBlock'
+            kryptoBlock.style.top = kryptoPosition + kryptoGap + 'vh'
+            kryptoBlock.style.left = '100vw'
+            kryptoBlock.increase_score = '1'
+
+            document.body.appendChild(kryptoBlock)
+        }
+        kryptoSeparation++
+        requestAnimationFrame(create_pipe)
     }
-    blockSeparation++
-    requestAnimationFrame(createBlock)
+    requestAnimationFrame(create_pipe)
 }
-requestAnimationFrame(createBlock)
-  
-}
-
